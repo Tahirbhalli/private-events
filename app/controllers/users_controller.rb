c55100f082c71e60dca1e63f72428490b06a0590
SpaYco <zizou.aziz101@gmail.com>
2020-05-04 14:52:38 +0100

    added views pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+    def create
+        @user = User.new(post_params)
+
+        if @user.save
+          redirect_to users_path
+        else
+          render :new
+        end
+    end
+    def show
+        @user = User.find(params[:id])
+    end
+    def new
+        @user = User.new
+    end
+    
+    private
+
+    def post_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+end
