e40244f62a5f99982f3229cdd0ebac5d7de822c0
tahirbhalli <tahirbhalli16@gmail.com>
2020-05-04 17:51:41 +0500

    models user

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+    def index
+        @users=User.all
+    end
+    def create
+        
+    end
+    def show
+        
+    end
+    def new
+
+    end
+    
+end
