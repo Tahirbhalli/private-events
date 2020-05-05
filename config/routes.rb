d828cb58db107eef5d1a2cf1f3b7dd84901e2096
tahirbhalli <tahirbhalli16@gmail.com>
2020-05-05 15:01:11 +0500

    sesion

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  match 'login' => 'users#login', via:[:get]
+  match 'session' => 'users#auth', via:[:post]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
