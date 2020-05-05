aeeef4249c5fb89b4226bbfbd8bd12323ff69e4a
SpaYco <zizou.aziz101@gmail.com>
2020-05-05 09:49:37 +0100

    re-created rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :users, only:[:index, :show, :new, :create, :login, :session]
-  get "/login" => "users#login"
-  post "/session" => "users#session"
-  root "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
