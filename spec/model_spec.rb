6a721b22a8a86fca4572e7c718744a192ae4c1f6
tahirbhalli <tahirbhalli16@gmail.com>
2020-05-08 13:28:24 +0500

    rspec coreected

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -9,12 +9,20 @@ describe 'dataase operations', type: :feature do
 
   it 'Create event without userid' do
     b = Event.new(title: 'conference', description: 'morning meeting')
-
-    expect(b.save).eql?(false)
+    b.save
+    expect(b.save).to eq(false)
   end
   it 'created before events' do
     b = Event.new(user_id: 1, title: 'abc', description: 'kdaj', event_date: '2029-09-06')
     b.save
-    expect(Event.created_before(Date.today).first.title).eql?('abc')
+    # a=Event.created_before(Date.today).first.select('title')
+    expect(Event.created_before(Date.today).first.title).to eq('abc')
+  end
+  it 'attendee scope method' do
+    b = Event.new(user_id: 1, title: 'abc', description: 'kdaj', event_date: '2021-09-06')
+    b.save
+    c = Attendee.new(user_id: 1, event_id: 1)
+    c.save
+    expect(Attendee.isjoined?(1, 1)).to eq(true)
   end
 end
